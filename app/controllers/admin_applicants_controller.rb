71276004d7bf008399ac52d9e1801a906ded68a6
loganjacob76 <nagolisback@gmail.com>
2021-04-08 16:28:34 -0600

    Render approve and reject buttons

--- app/controllers/admin_applicants_controller.rb
+++ app/controllers/admin_applicants_controller.rb
@@ -1,6 +1,5 @@
 class AdminApplicantsController < ApplicationController
     def show
         @applicant = Applicant.find(params[:id])
-        binding.pry
     end
 end
--- spec/features/admin_applicants/show_spec.rb
+++ spec/features/admin_applicants/show_spec.rb
@@ -3,13 +3,11 @@ require 'rails_helper'
 RSpec.describe 'admin applicant show page' do
     before :each do
         @john = Applicant.create!(name: 'John Doe', street: '1234 Example Dr.', city: 'Denver', state: 'CO', zip: 12345, personal_statement: 'I love animals and live next to a park.')
-        @jane = Applicant.create!(name: 'Jane Doe', street: '1234 Example Dr.', city: 'Denver', state: 'CO', zip: 12345, personal_statement: 'I love animals and live next to a park.')
         @shelter1 = Shelter.create!(foster_program: true, name: 'Paws of Life', city: 'Denver', rank: 3)
         @pet1 = @shelter1.pets.create!(adoptable: true, age: 1, breed: 'Labrador', name: 'Star')
         @pet2 = @shelter1.pets.create!(adoptable: true, age: 4, breed: 'Mutt', name: 'Boo')
 
         AdoptionApplication.create!(applicant_id: @john.id, pet_id: @pet1.id)
-        AdoptionApplication.create!(applicant_id: @jane.id, pet_id: @pet1.id)
         AdoptionApplication.create!(applicant_id: @john.id, pet_id: @pet2.id)
     end
 
