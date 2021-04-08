345ac5b2f3158d81c75db3aaa051c67388cd6afb
loganjacob76 <nagolisback@gmail.com>
2021-04-07 19:11:12 -0600

    Set up tests

--- spec/features/applicants/show_spec.rb
+++ spec/features/applicants/show_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'applicants show page' do
     before :each do
         @john = Applicant.create!(name: 'John Doe', street: '1234 Example Dr.', city: 'Denver', state: 'CO', zip: 12345, personal_statement: 'I love animals and live next to a park.')
+        @jane = Applicant.create!(name: 'Jane Doe', street: '1234 Example Dr.', city: 'Denver', state: 'CO', zip: 12345, personal_statement: 'I love animals and live next to a park.')
         @shelter1 = Shelter.create!(foster_program: true, name: 'Paws of Life', city: 'Denver', rank: 3)
         @pet1 = @shelter1.pets.create!(adoptable: true, age: 1, breed: 'Labrador', name: 'Star')
         @pet2 = @shelter1.pets.create!(adoptable: true, age: 4, breed: 'Mutt', name: 'Boo')
@@ -16,12 +17,16 @@ RSpec.describe 'applicants show page' do
         visit "/applicants/#{@john.id}"
 
         expect(page).to have_content(@john.name)
-        expect(page).to have_content(@john.street)
-        expect(page).to have_content(@john.city)
-        expect(page).to have_content(@john.state)
-        expect(page).to have_content(@john.zip)
+        within 'div#address' do
+            expect(page).to have_content(@john.street)
+            expect(page).to have_content(@john.city)
+            expect(page).to have_content(@john.state)
+            expect(page).to have_content(@john.zip)
+        end
         expect(page).to have_content(@john.personal_statement)
-        expect(page).to have_content(@john.status)
+        within 'p#status' do
+            expect(page).to have_content('Application Status: In Progress')
+        end
     end
 
     it 'lists all pets being applied for as links to their show pages' do
@@ -66,6 +71,37 @@ RSpec.describe 'applicants show page' do
 
             expect(current_path).to eq("/applicants/#{@john.id}")
             expect(page).to have_link(href: "/pets/#{@pet3.id}")
+        end
+    end
+
+    describe 'submitting an application' do
+        it 'must have pets to be submittable' do
+            visit "/applicants/#{@jane.id}"
+
+            expect(page).to_not have_button('Submit Application')
+
+            fill_in :name, with: 'Peter Barker'
+            click_button 'Submit'
+
+            click_button 'Adopt this Pet'
+
+            expect(page).to have_button('Submit Application')
+        end
+
+        it 'Once it is submitted it shows all desired pets and no more can be added' do
+            visit "/applicants/#{@jane.id}"
+
+            fill_in :name, with: 'Peter Barker'
+            click_button 'Submit'
+
+            click_button 'Adopt this Pet'
+            click_button 'Submit Application'
+
+            expect(current_path.to eq("/applicants/#{@jane.id}")
+            expect(page).to have_content(@pet3.name)
+            within '#status' do
+                expect(page).to have_content('Application Status: Pending')
+            end
         end
     end
 end
